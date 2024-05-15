6b2c656955c2ea143400f8fdccc6c36fcb6d4257
Rupam Dey <101353371+rupam241@users.noreply.github.com>
2024-05-15 19:27:10 +0530

    Add files via upload

---
+++ popup/script.js
@@ -0,0 +1,11 @@
+const openPop=document.getElementById('popup')
+const closePop=document.getElementById('close');
+
+
+const openPopup=()=>{
+    openPop.classList.add('openPopUpWith')
+}
+const closePopUp=()=>{
+
+    openPop.classList.remove('openPopUpWith')
+}
